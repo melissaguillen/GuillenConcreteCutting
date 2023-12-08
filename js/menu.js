bb9eae39e5a0e9d0410010aff3e77e87dbc53e10
mguillen4 <mguillen4@twu.edu>
2023-12-08 13:57:24 -0600

    responsive tables, adjusting css

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,23 @@
 var menuButton = document.getElementById("hamburger");
 var menu = document.getElementById("menu");
+// var links =document.getElementsByClassName("menuLinks");
 
 menuButton.addEventListener("click", function() {
     menu.classList.toggle("active");
 });
+
+function addingEventToAll() {
+    let links =document.getElementsByClassName("menuLinks");
+    for (let i = 0; i < links.length; i++) {
+        links[i].addEventListener("click", function() {
+                for (let j = 0; j < links.length; j++){
+                    links[j].classList.remove("selected");
+                }
+                this.classList.add("selected"); 
+        });
+    }
+};
+// let links =document.getElementsByClassName("menuLinks");
+// for (let i = 0; i < links.length; i++) {
+//     links[i].classList.remove("selected");
+// };
