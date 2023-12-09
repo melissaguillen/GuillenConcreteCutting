97d9dba8bc469df95b9d33fd7e50e8417133df9d
mguillen4 <mguillen4@twu.edu>
2023-12-08 20:34:31 -0600

    simple approach

--- js/dynamicThanks.js
+++ js/dynamicThanks.js
@@ -1,11 +1,12 @@
-let fullname = document.getElementById("name");
-let submit = document.getElementById("submit");
+//let fullname = document.getElementById("name");
+// let submit = document.getElementById("submit");
 let h1 = document.getElementById("dynamicThanksies");
 
 
-submit.addEventListener("click", function(){
-   var thankU = localStorage.setItem("fullname", fullname.value);
-});
-    var Tname = localStorage.getItem("fullname");
-    var text = document.createTextNode("Thank You, "+Tname+ "!")
+// submit.addEventListener("click", function(){
+//    var thankU = localStorage.setItem("fullname", fullname.value);
+// });
+    //var Tname = localStorage.getItem("fullname");
+   // var text = document.createTextNode("Thank You, "+Tname+ "!")
+    var text = document.createTextNode("Thank You!")
     h1.appendChild(text);
