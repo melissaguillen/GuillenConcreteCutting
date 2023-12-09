ad7b57c1f23e3ecd4a1fe44d613bd23d09a3cd80
mguillen4 <mguillen4@twu.edu>
2023-12-08 20:09:27 -0600

    test js

--- js/dynamicThanks.js
+++ js/dynamicThanks.js
@@ -2,7 +2,7 @@ let fullname = document.getElementById("name");
 let submit = document.getElementById("submit");
 let h1 = document.getElementById("dynamicThanksies");
 
-submit.addEventListener("submit", function(){
+submit.addEventListener("click", function(){
     let thankU = localStorage.setItem(fullname.value);
     h1.innerHTML = "Thank You, " +thankU+ "!";
 });
