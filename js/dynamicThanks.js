d8e005cc33a9095af1284493b6e53c8d9ab8a053
mguillen4 <mguillen4@twu.edu>
2023-12-08 20:16:23 -0600

    a step at a time

--- js/dynamicThanks.js
+++ js/dynamicThanks.js
@@ -1,8 +1,10 @@
 let fullname = document.getElementById("name");
 let submit = document.getElementById("submit");
 let h1 = document.getElementById("dynamicThanksies");
+let thankU;
 
-submit.addEventListener("click", function(){
-    let thankU = fullname.value;
-    h1.innerHTML = "Thank You, " +thankU+ "!";
-});
+// submit.addEventListener("click", function(){
+//     thankU = fullname.value;
+// });
+h1.innerHTML = "Thank You!";
+    // h1.innerHTML = "Thank You, " +thankU+ "!";
